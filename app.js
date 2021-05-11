fb7db816b181dff30c69edd7f919d8932b72dc5c
Yusha <yushaushaibu1@gmail.com>
2021-05-11 13:38:14 +0000

    Local storage added

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = Number(movieSelect.value);
 
+// Save selected Movie index and price
+function setMovieData(movieIndex, priceIndex) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', priceIndex);
+}
+
+
 // Update count and total
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');  // Grabs only selected seats
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');  // Grabs only selected seats.
+
+    const seatIndex = [...selectedSeats].map(function(seat) {  //Grabs selected seats, loops through all seats and return selected seats index.
+        return [...seats].indexOf(seat)
+    });
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));  // Converts selected seats index into string and stores in local storage.
+
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;   
     total.innerText = ticketPrice * selectedSeatsCount; 
@@ -17,6 +32,8 @@ function updateSelectedCount() {
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = Number(e.target.value);
+    // console.log(e.target.selectedIndex, e.target.value);  //Shows the selected movie index and price.
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
